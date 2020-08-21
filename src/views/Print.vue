328bf770b09db2c9a4e1eeaeef52bb233db619f7
Zubs <zubairidrisaweda@gmail.com>
2020-08-22 00:09:53 +0100

    I guess I'm set but can't test sadly cause of a bug that I'm not gonna lose my life to yet

--- src/views/Print.vue
+++ src/views/Print.vue
@@ -8,28 +8,55 @@
         ><button><b>Back to Homepage</b></button></router-link
       >
     </div>
-    <form action="" @submit.prevent="handleSubmit" class="fadeRight" :class="success ? 'hide' : ''">
+    <form
+      action=""
+      @submit.prevent="handleSubmit"
+      class="fadeRight"
+      :class="success ? 'hide' : ''"
+    >
       <span class="err">{{ error }}</span>
       <div>
         <label for="name" class="active">Name</label>
-        <input v-model="name" name="Name" type="text" placeholder="Enter Your Full Name" required />
+        <input
+          v-model="name"
+          name="Name"
+          type="text"
+          placeholder="Enter Your Full Name"
+          required
+        />
       </div>
 
       <div>
         <label for="phone">Phone Num</label>
-        <input v-model="number" name="Phone" type="text" placeholder="WhatsApp Prefered" required />
+        <input
+          v-model="number"
+          name="Phone"
+          type="text"
+          placeholder="WhatsApp Prefered"
+          required
+        />
       </div>
 
       <div style=" margin-bottom: 8px;">
         <label for="">Urgent?</label>
-        <select v-model="urgent" name="Colored" required style="display: block;">
+        <select
+          v-model="urgent"
+          name="Colored"
+          required
+          style="display: block;"
+        >
           <option value="No" selected>No</option>
           <option value="Yes">Yes</option>
         </select>
       </div>
       <div style=" margin-bottom: 8px;">
         <label for="">Colored?</label>
-        <select v-model="colored" name="Colored" required style="display: block;">
+        <select
+          v-model="colored"
+          name="Colored"
+          required
+          style="display: block;"
+        >
           <option value="No" selected>No</option>
           <option value="Yes">Yes</option>
         </select>
@@ -37,7 +64,12 @@
 
       <div style=" margin-bottom: 8px;">
         <label for="">Pick Up?</label>
-        <select v-model="pickup" name="Faculty" required="" style="display: block;">
+        <select
+          v-model="pickup"
+          name="Faculty"
+          required=""
+          style="display: block;"
+        >
           <option value="No" selected>No</option>
           <option value="Yes" selected>Yes</option>
         </select>
@@ -45,21 +77,47 @@
 
       <div>
         <label for="dept">Description</label>
-        <textarea v-model="description" name="Department" type="text" placeholder="Enter Detailed description on how you want the document" required></textarea>
+        <textarea
+          v-model="description"
+          name="Department"
+          type="text"
+          placeholder="Enter Detailed description on how you want the document"
+          required
+        ></textarea>
       </div>
 
       <div>
         <label for="book" class="upload" v-if="price">#{{ price }}</label>
-        <input @change="update" class="custom-upload__input" id="book" type="file" style="display: none;" data-behaviour="custom-upload-input" name="file" required="" />
+        <input
+          @change="update"
+          class="custom-upload__input"
+          id="book"
+          type="file"
+          style="display: none;"
+          data-behaviour="custom-upload-input"
+          name="file"
+          required=""
+        />
       </div>
 
       <div>
         <label for="book" id="uploadbtn" class="upload">{{ title }}</label>
-        <input @change="update" class="custom-upload__input" id="book" type="file" style="display: none;" data-behaviour="custom-upload-input" name="file" required="" />
+        <input
+          @change="update"
+          class="custom-upload__input"
+          id="book"
+          type="file"
+          style="display: none;"
+          data-behaviour="custom-upload-input"
+          name="file"
+          required=""
+        />
       </div>
 
       <div class="btnC">
-        <button type="submit" id="btn" style="margin-top: 50px;">{{ print }}</button>
+        <button type="submit" id="btn" style="margin-top: 50px;">
+          {{ print }}
+        </button>
         <button id="btn" style="margin-top: 50px;">Save</button>
       </div>
     </form>
@@ -67,292 +125,305 @@
 </template>
 
 <script>
-  import firebase from 'firebase/app';
-  import 'firebase/firestore';
-  import 'firebase/storage';
-  import 'firebase/auth';
-  // import 'docx-pdf-pagecount';
-  const user = firebase.auth().currentUser;
-  const db = firebase.firestore();
-  const up = db.collection('users').doc(user.uid);
-  export default {
-    data() {
-      return {
-        success: false,
-        print: 'Print',
-        title: 'Upload',
-        name: '',
-        number: '',
-        urgent: 'No',
-        colored: 'No',
-        pickup: 'Yes',
-        description: '',
-        error: '',
-        price: '',
-        pages: ''
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/storage";
+import "firebase/auth";
+// import 'docx-pdf-pagecount';
+const user = firebase.auth().currentUser;
+const db = firebase.firestore();
+const up = db.collection("users").doc(user.uid);
+export default {
+  data() {
+    return {
+      success: false,
+      print: "Print",
+      title: "Upload",
+      name: "",
+      number: "",
+      urgent: "No",
+      colored: "No",
+      pickup: "Yes",
+      description: "",
+      error: "",
+      price: "",
+      pages: ""
+    };
+  },
+  created() {
+    this.success = false;
+  },
+  methods: {
+    // getPrice(pdf) {
+    //   const reader = new FileReader();
+    //   reader.readAsBinaryString(pdf);
+    //   reader.onloadend = function() {
+    //     const count = reader.result.match(/\/Type[\s]*\/Page[^s]/g).length;
+    //     console.log('Number of Pages:',count );
+    //     this.pages = count;
+    //   }
+    //   if (this.colored === 'Yes') {
+    //     if (this.pages > 10) {
+    //       this.price = 30 * this.pages;
+    //     } else {
+    //       this.price = 35 * this.pages;
+    //     }
+    //   } else {
+    //     if (this.pages > 10) {
+    //       this.price = 10 * this.pages;
+    //     } else {
+    //       this.price = 15 * this.pages;
+    //     }
+    //   };  
+    // },
+    handleSubmit() {
+      const object = {
+        time: `${new Date().toLocaleTimeString()} of ${new Date().toLocaleDateString()}`,
+        status: "In-progress",
+        user: user.uid,
+        title: this.title,
+        name: this.name,
+        urgent: this.urgent,
+        number: this.number,
+        colored: this.colored,
+        pickup: this.pickup,
+        description: this.description
       };
-    },
-    created() {
-      this.success = false;
-    },
-    methods: {
-      handleSubmit() {
-        const object = {
-          time:`${new Date().toLocaleTimeString()} of ${new Date().toLocaleDateString()}`,
-          status:'In-progress',
-          user: user.uid,
-          title: this.title,
-          name: this.name,
-          urgent: this.urgent,
-          number: this.number,
-          colored: this.colored,
-          pickup: this.pickup,
-          description: this.description
-        };
-        this.print = 'Loading...';
-        console.log(document.getElementById('book').files[0]);
-
-        /*
-        if (this.colored === 'Yes') {
-          if (this.pages > 10) {
-            this.price = 30 * this.pages;
-          } else {
-            this.price = 35 * this.pages;
-          }
-        } else {
-          if (this.pages > 10) {
-            this.price = 10 * this.pages;
-          } else {
-            this.price = 15 * this.pages;
-          }
-        };
-        */
-
-        const ref = db.collection('toPrint').doc(`${this.title} by ${this.name}`);
-        console.log(this.title, this.name, this.number, this.colored, this.pickup, this.description);
-        ref
-          .set(object)
-          .then(() => {
-            up.update({
-              inProgress: firebase.firestore.FieldValue.arrayUnion(object)
-            });
-          })
-          .then(() => {
-            console.log('UPLOAD-FILE called!');
-            const storageReference = firebase.storage().ref();
-            const file = document.getElementById('book').files[0];
-            const uploadTask = storageReference
-              .child('toPrint/' + `${this.title} by ${this.name}`)
-              .put(file)
-              .then(() => {
-                this.success = true;
-              });
-          })
-          .catch((err) => {
-            this.error = err.message;
+      this.print = "Loading...";
+      console.log(document.getElementById("book").files[0]);
+      // this.getPrice(document.getElementById("book").files[0]);
+      const ref = db.collection("toPrint").doc(`${this.title} by ${this.name}`);
+      console.log(
+        this.title,
+        this.name,
+        this.number,
+        this.colored,
+        this.pickup,
+        this.description
+      );
+      ref
+        .set(object)
+        .then(() => {
+          up.update({
+            inProgress: firebase.firestore.FieldValue.arrayUnion(object)
           });
-      },
-      update(evt) {
-        const input = evt.currentTarget.value.split('\\').pop();
-        console.log(input);
-        this.title = input;
-      }
+        })
+        .then(() => {
+          console.log("UPLOAD-FILE called!");
+          const storageReference = firebase.storage().ref();
+          const file = document.getElementById("book").files[0];
+          const uploadTask = storageReference
+            .child("toPrint/" + `${this.title} by ${this.name}`)
+            .put(file)
+            .then(() => {
+              this.success = true;
+            });
+        })
+        .catch(err => {
+          this.error = err.message;
+        });
+    },
+    update(evt) {
+      const input = evt.currentTarget.value.split("\\").pop();
+      console.log(input);
+      this.title = input;
     }
-  };
+  }
+};
 </script>
 
 <style scoped>
-  .hide {
-    display: none;
-  }
-  p {
-    color: green;
-  }
-  img {
-    height: auto;
-    max-width: 50%;
-  }
-  .err {
-    color: red;
-  }
-  .btnC {
-    width: 60vw;
-    display: flex;
-    justify-content: space-between;
-  }
-  form {
-    padding: 10px;
-    margin: 0px !important;
-    width: 50vw;
-    text-align: start;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
-  select {
-    background-color: rgba(255, 255, 255, 0.9);
-    width: 65vw;
-    padding: 5px;
-    border: 1px solid #f2f2f2;
-    border-radius: 2px;
-    height: 3rem;
-    outline: none;
-  }
-  input {
-    background-color: transparent;
-    border: none;
-    border-bottom: 1px solid #9e9e9e;
-    border-radius: 0;
-    outline: none;
-    height: 3rem;
-    width: 65vw;
-    font-size: 16px;
-    margin: 0 0 8px 0;
-    padding: 0;
-    box-shadow: none;
-    -webkit-writing-mode: horizontal-tb !important;
-    text-rendering: auto;
-    color: -internal-light-dark(black, white);
-    letter-spacing: normal;
-    word-spacing: normal;
-    text-transform: none;
-    text-indent: 0px;
-    text-shadow: none;
-    display: inline-block;
-    text-align: start;
-    appearance: textfield;
-  }
+.hide {
+  display: none;
+}
+p {
+  color: green;
+}
+img {
+  height: auto;
+  max-width: 50%;
+}
+.err {
+  color: red;
+}
+.btnC {
+  width: 60vw;
+  display: flex;
+  justify-content: space-between;
+}
+form {
+  padding: 10px;
+  margin: 0px !important;
+  width: 50vw;
+  text-align: start;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+select {
+  background-color: rgba(255, 255, 255, 0.9);
+  width: 65vw;
+  padding: 5px;
+  border: 1px solid #f2f2f2;
+  border-radius: 2px;
+  height: 3rem;
+  outline: none;
+}
+input {
+  background-color: transparent;
+  border: none;
+  border-bottom: 1px solid #9e9e9e;
+  border-radius: 0;
+  outline: none;
+  height: 3rem;
+  width: 65vw;
+  font-size: 16px;
+  margin: 0 0 8px 0;
+  padding: 0;
+  box-shadow: none;
+  -webkit-writing-mode: horizontal-tb !important;
+  text-rendering: auto;
+  color: -internal-light-dark(black, white);
+  letter-spacing: normal;
+  word-spacing: normal;
+  text-transform: none;
+  text-indent: 0px;
+  text-shadow: none;
+  display: inline-block;
+  text-align: start;
+  appearance: textfield;
+}
 
-  textarea {
-    resize: vertical;
-    background-color: transparent;
-    border: none;
-    border-bottom: 1px solid #9e9e9e;
-    border-radius: 0;
-    outline: none;
-    height: 3rem;
-    width: 65vw;
-    font-size: 16px;
-    margin: 0 0 8px 0;
-    padding: 0;
-    box-shadow: none;
-    -webkit-writing-mode: horizontal-tb !important;
-    text-rendering: auto;
-    color: -internal-light-dark(black, white);
-    letter-spacing: normal;
-    word-spacing: normal;
-    text-transform: none;
-    text-indent: 0px;
-    text-shadow: none;
-    display: inline-block;
-    text-align: start;
-    appearance: textfield;
-  }
-  input:focus {
-    border-bottom: 1px solid var(--primary-color);
-  }
-  textarea:focus {
-    border-bottom: 1px solid var(--primary-color);
-  }
-  label {
-    cursor: default;
-    color: #9e9e9e;
-    font-size: larger;
-    display: block;
-  }
-  .con {
-    margin-top: 5rem !important;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 2rem;
-  }
-  .upload {
-    text-transform: uppercase;
-    margin: 5px 0px !important;
-    padding: 0.5rem;
-    color: var(--primary-color);
-    background-color: transparent;
-    outline: none;
-    border-radius: 5px;
-    border: 1px solid var(--background-color);
-    width: fit-content;
-  }
+textarea {
+  resize: vertical;
+  background-color: transparent;
+  border: none;
+  border-bottom: 1px solid #9e9e9e;
+  border-radius: 0;
+  outline: none;
+  height: 3rem;
+  width: 65vw;
+  font-size: 16px;
+  margin: 0 0 8px 0;
+  padding: 0;
+  box-shadow: none;
+  -webkit-writing-mode: horizontal-tb !important;
+  text-rendering: auto;
+  color: -internal-light-dark(black, white);
+  letter-spacing: normal;
+  word-spacing: normal;
+  text-transform: none;
+  text-indent: 0px;
+  text-shadow: none;
+  display: inline-block;
+  text-align: start;
+  appearance: textfield;
+}
+input:focus {
+  border-bottom: 1px solid var(--primary-color);
+}
+textarea:focus {
+  border-bottom: 1px solid var(--primary-color);
+}
+label {
+  cursor: default;
+  color: #9e9e9e;
+  font-size: larger;
+  display: block;
+}
+.con {
+  margin-top: 5rem !important;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 2rem;
+}
+.upload {
+  text-transform: uppercase;
+  margin: 5px 0px !important;
+  padding: 0.5rem;
+  color: var(--primary-color);
+  background-color: transparent;
+  outline: none;
+  border-radius: 5px;
+  border: 1px solid var(--background-color);
+  width: fit-content;
+}
 
-  .upload:hover {
-    margin: 5px 0px !important;
-    padding: 0.5rem;
-    color: white;
-    background-color: var(--primary-color);
-    outline: none;
-    border-radius: 5px;
-    border: transparent;
-    width: fit-content;
-  }
-  main button {
-    text-transform: uppercase;
-    margin: 5px 0px !important;
-    padding: 0.5rem;
-    color: var(--primary-color);
-    background-color: transparent;
-    outline: none;
-    border-radius: 5px;
-    border: 1px solid var(--background-color);
-    width: fit-content;
-  }
+.upload:hover {
+  margin: 5px 0px !important;
+  padding: 0.5rem;
+  color: white;
+  background-color: var(--primary-color);
+  outline: none;
+  border-radius: 5px;
+  border: transparent;
+  width: fit-content;
+}
+main button {
+  text-transform: uppercase;
+  margin: 5px 0px !important;
+  padding: 0.5rem;
+  color: var(--primary-color);
+  background-color: transparent;
+  outline: none;
+  border-radius: 5px;
+  border: 1px solid var(--background-color);
+  width: fit-content;
+}
 
-  main button:hover {
-    margin: 5px 0px !important;
-    padding: 1rem;
-    color: white;
-    background-color: var(--primary-color);
-    outline: none;
-    border-radius: 5px;
-    border: transparent;
-    width: fit-content;
-  }
+main button:hover {
+  margin: 5px 0px !important;
+  padding: 1rem;
+  color: white;
+  background-color: var(--primary-color);
+  outline: none;
+  border-radius: 5px;
+  border: transparent;
+  width: fit-content;
+}
 
-  * {
-    transition: 0.3s linear;
-  }
+* {
+  transition: 0.3s linear;
+}
 
+main {
+  background-color: white;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  letter-spacing: 1px;
+  word-spacing: 0.4rem;
+}
+
+@media screen and (max-width: 813px) {
+  body {
+    overflow: auto;
+  }
   main {
-    background-color: white;
-    height: 100vh;
     width: 100vw;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-    letter-spacing: 1px;
-    word-spacing: 0.4rem;
+    overflow-y: auto;
   }
+  main {
+    width: 100vw;
+    margin-bottom: 4rem !important;
+    overflow-y: auto;
+  }
+}
 
-  @media screen and (max-width: 813px) {
-    body {
-      overflow: auto;
-    }
-    main {
-      width: 100vw;
-      overflow-y: auto;
-    }
-    main {
-      width: 100vw;
-      margin-bottom: 4rem !important;
-      overflow-y: auto;
-    }
+@media screen and (max-width: 613px) {
+  main {
+    overflow-y: auto;
   }
-
-  @media screen and (max-width: 613px) {
-    main {
-      overflow-y: auto;
-    }
-    main {
-      margin-bottom: 4rem !important;
-      overflow-y: auto;
-    }
+  main {
+    margin-bottom: 4rem !important;
+    overflow-y: auto;
   }
+}
 </style>
